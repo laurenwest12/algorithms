f6e89ea18c823a1abcadb05219b9fb9546d4a7ab
Lauren West <laurenwest12@gmail.com>
2023-05-11 16:17:10 -0400

    Implemented queue

---
+++ data structures/queue.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val) {
+    let node = new Node(val);
+    if (this.size === 0) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+
+    this.size++;
+    return this.size;
+  }
+
+  dequeue() {
+    let first = this.first;
+    if (this.size === 0) return undefined;
+    if (this.first === this.last) {
+      this.first = null;
+      this.last = null;
+    }
+    this.first = first.next;
+    first.next = null;
+    return first;
+  }
+}
+
+let q = new Queue();
+q.enqueue('McDouble');
+q.enqueue('McChicken');
+q.enqueue('McFlurry');
+console.log(q);
+q.dequeue();
+console.log(q);
