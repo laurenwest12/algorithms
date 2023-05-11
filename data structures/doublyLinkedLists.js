5387c911f86418cd49950f6e6e7419ac46ecd268
Lauren West <laurenwest12@gmail.com>
2023-05-11 11:54:35 -0400

    Added pop, shift, and unshift methods

--- data structures/doublyLinkedLists.js
+++ data structures/doublyLinkedLists.js
@@ -12,4 +12,87 @@ class DoublyLinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  push(val) {
+    let node = new Node(val);
+
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+      this.length++;
+      return this;
+    }
+
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length === 0) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      newTail.next = null;
+      poppedNode.prev = null;
+    }
+
+    this.length--;
+    return poppedNode;
+  }
+
+  shift() {
+    if (this.length === 0) return undefined;
+
+    let shiftedNode = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = shiftedNode.next;
+      this.head.prev = null;
+      shiftedNode.next = null;
+    }
+    this.length--;
+    return shiftedNode;
+  }
+
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+let list = new DoublyLinkedList();
+// list.push('Hello');
+// list.push('there');
+// list.push('Lauren!');
+
+// list.pop();
+// list.pop();
+// list.pop();
+// console.log(list);
+
+// list.shift();
+// list.shift();
+// list.shift();
+// console.log(list);
+
+// list.unshift('Lauren');
+// list.unshift('there');
+// list.unshift('Hello');
+// console.log(list);
