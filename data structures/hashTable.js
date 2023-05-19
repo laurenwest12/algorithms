75b0774d20d21ee4e92ec95d5ff1321a337d5d8c
Lauren West <laurenwest12@gmail.com>
2023-05-19 11:09:43 -0400

    Implemented keys and values on HashTable

--- data structures/hashTable.js
+++ data structures/hashTable.js
@@ -35,6 +35,38 @@ class HashTable {
 
     return undefined;
   }
+
+  keys() {
+    let keys = [];
+
+    for (let i = 0; i < this.keyMap.length; ++i) {
+      const pairs = this.keyMap[i];
+      if (pairs) {
+        for (let j = 0; j < pairs.length; ++j) {
+          const key = pairs[j][0];
+          if (!keys.includes(key)) keys.push(key);
+        }
+      }
+    }
+
+    return keys;
+  }
+
+  values() {
+    let values = [];
+
+    for (let i = 0; i < this.keyMap.length; ++i) {
+      const pairs = this.keyMap[i];
+      if (pairs) {
+        for (let j = 0; j < pairs.length; ++j) {
+          const value = pairs[j][1];
+          if (!values.includes(value)) values.push(value);
+        }
+      }
+    }
+
+    return values;
+  }
 }
 
 let table = new HashTable();
@@ -42,9 +74,15 @@ table.set('cyan', '#00FFFF');
 table.set('red', '#FF0000');
 table.set('pink', '#FFC0CB');
 table.set('lavender', '#E6E6FA');
+table.set('purple', '#E6E6FA');
+table.set('lavender', '#E6E6FA');
 const k1 = table.get('lavender');
 const k2 = table.get('salmon');
 const k3 = table.get('periwinkle');
 const k4 = table.get('red');
 
-console.log(k1, k2, k3, k4);
+const values = table.values();
+console.log(values);
+
+const keys = table.keys();
+console.log(keys);
