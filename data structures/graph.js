db3b1522a5a5f624f892fd98d22303d29112deb1
Lauren West <laurenwest12@gmail.com>
2023-05-19 14:55:17 -0400

    Added method for adding edge to Graph

--- data structures/graph.js
+++ data structures/graph.js
@@ -6,9 +6,20 @@ class Graph {
   addVertex(name) {
     if (!this.adjacencyList[name]) this.adjacencyList[name] = [];
   }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
 }
 
 let graph = new Graph();
 graph.addVertex('Tokyo');
+graph.addVertex('NYC');
+graph.addVertex('Dallas');
+graph.addVertex('Atlanta');
+graph.addVertex('Cleveland');
 
+graph.addEdge('NYC', 'Atlanta');
+graph.addEdge('NYC', 'Cleveland');
 console.log(graph);
