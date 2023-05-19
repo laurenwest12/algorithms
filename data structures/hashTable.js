1c4eff8071c21055ee0170f1a97e78818b412016
Lauren West <laurenwest12@gmail.com>
2023-05-19 10:45:51 -0400

    Fixed hashing function

--- data structures/hashTable.js
+++ data structures/hashTable.js
@@ -10,8 +10,18 @@ class HashTable {
     for (let i = 0; i < Math.min(key.length, 100); ++i) {
       let char = key[i];
       let value = char.charCodeAt(0) - 96;
-      total += (total * PRIME * value) % this.keyMap.length;
+      total = (total * PRIME + value) % this.keyMap.length;
     }
     return total;
   }
+
+  set(key, value) {
+    const hash = this._hash(key);
+  }
+
+  get() {}
 }
+
+let table = new HashTable();
+table.set('red', '#12345');
+table.set('salmon', '#67890');
