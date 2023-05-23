0216d4d2233c47251b1dedfebe61f2454cf60015
Lauren West <laurenwest12@gmail.com>
2023-05-23 16:57:17 -0400

    initialized priority queue in weightedgraph

--- data structures/weightedGraph.js
+++ data structures/weightedGraph.js
@@ -13,13 +13,38 @@ class WeightedGraph {
   }
 }
 
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+
+  dequeue() {
+    return this.values.shift();
+  }
+
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
 let graph = new WeightedGraph();
 graph.addVertex('A');
 graph.addVertex('B');
 graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
 
-graph.addEdge('A', 'B', 9);
-graph.addEdge('A', 'C', 5);
-graph.addEdge('B', 'C', 7);
-
-console.log(graph.adjacencyList['A']);
+graph.addEdge('A', 'B', 4);
+graph.addEdge('A', 'C', 2);
+graph.addEdge('B', 'E', 3);
+graph.addEdge('C', 'D', 2);
+graph.addEdge('C', 'F', 4);
+graph.addEdge('D', 'E', 3);
+graph.addEdge('D', 'F', 1);
+graph.addEdge('E', 'F', 1);
