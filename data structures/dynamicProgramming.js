1e0ee71bcca4853bde7c457fe40c778ccaa6f7ce
Lauren West <laurenwest12@gmail.com>
2023-05-24 11:13:37 -0400

    Added tabulation approac for fib

--- data structures/dynamicProgramming.js
+++ data structures/dynamicProgramming.js
@@ -1,9 +1,21 @@
-const fib = (n, memo = []) => {
+// memoization
+const fibMemo = (n, memo = []) => {
   if (memo[n] !== undefined) return memo[n];
   if (n <= 2) return 1;
-  const res = fib(n - 1, memo) + fib(n - 2, memo);
+  const res = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
   memo[n] = res;
   return res;
 };
 
-console.log(fib(3));
+console.log(fibMemo(3));
+
+// tabulation
+
+const fibTab = (n) => {
+  if (n <= 2) return 1;
+  let fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
+};
